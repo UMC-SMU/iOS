d7d63c7742422134239ba22b30210f4beb083130
J.H.Moon <moonjh587@naver.com>
2022-05-08 04:06:57 +0900

    modified commit

---
+++ Catstagram/Catstagram/Profile/Cell/ProfileCell/ProfileCollectionViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileCollectionViewCell.swift
+//  Catstagram
+//
+//  Created by 김유빈 on 2022/04/30.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ProfileCollectionViewCell"
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var addProfileImageView: UIImageView!
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    @IBOutlet weak var addFriendButton: UIButton!
+    
+    @IBOutlet weak var postingCountLabel: UILabel!
+    
+    @IBOutlet weak var followerCountLabel: UILabel!
+    
+    @IBOutlet weak var followingCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupAttribute()
+    }
+    
+    private func setupAttribute() {
+        profileImageView.layer.cornerRadius = 88 / 2
+        addProfileImageView.layer.cornerRadius = 24 / 2
+        
+        profileImageView.layer.borderColor = UIColor.darkGray.cgColor
+        profileImageView.layer.borderWidth = 1
+        
+        editButton.layer.cornerRadius = 5
+        editButton.layer.borderColor = UIColor.lightGray.cgColor
+        editButton.layer.borderWidth = 1
+        
+        addFriendButton.layer.cornerRadius = 3
+        addFriendButton.layer.borderColor = UIColor.lightGray.cgColor
+        addFriendButton.layer.borderWidth = 1
+        
+        [postingCountLabel, followerCountLabel, followingCountLabel]
+            .forEach { $0.text = "\(Int.random(in: 0...10))"}
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,23 +1,2 @@
-
-<div align="center">
-  <img src="https://user-images.githubusercontent.com/50178026/159714572-6ec0fdca-2ce1-4611-8d27-c53e10b32fd5.png" width="16%" height="16%"/>
- <h2> 
- <i> 🍎 iOS Study</i> 
- </h2>
-</div>  
-
-### 📝 스터디원 
-|이름|전공|
-|------|---|
-|[니니(김유빈)](https://github.com/ubeeni)|👩🏻‍🎓 SMU 휴먼지능정보공학전공 4학년|
-|[무너(문정호)](https://github.com/ILWAT)|🧑🏻‍🎓 SMU 컴퓨터과학전공 3학년|
-|[윶(박유주)](https://github.com/yuju-SMU)|👩🏻‍🎓 SMU 게임전공 3학년|
-
-### 💬 커리큘럼
-+ **환경설정 및 언어소개**
-+ **화면구성 및 기능구현**
-+ **라이브러리를 활용한 서버 기능 연동**
-+ **네트워킹 개요**
-+ **Alamofire**
-+ **화면 연동**
-+ **기능 응용**
+# iOS
+🍎 UMC 2기 iOS 스터디
